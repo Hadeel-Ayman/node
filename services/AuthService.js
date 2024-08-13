8d5b96fa254ad4b3ec0d29b0d1c2854b9ec0017e
Hadeel-Ayman <haddelayman4@gmail.com>
2024-08-14 02:37:52 +0300

    add chnge

--- services/AuthService.js
+++ services/AuthService.js
@@ -21,29 +21,20 @@ const createJWT = (user, message, statusCode, res) => {
         });
 };
 
-
-const encryptPassword = async (password) => {
-    return await bcrypt.hash(password, 8);
-};
-
-// دالة للتحقق من تطابق كلمة المرور
-const comparePassword = async (password, hashedPassword) => {
-    return await bcrypt.compare(password, hashedPassword);
-};
-
 // دالة تسجيل الدخول
 exports.login = asyncHandler(async (req, res, next) => {
     try {
         const { email, password } = req.body;
         const user = await User.findOne({ email });
-        if (!user || !(await comparePassword(password, user.password))) {
+        if (!user || password !== user.password) {
             return next(new ApiError('Invalid email or password', 401));
         }
-        res.json("succefully login");
+        res.json("successfully logged in");
     } catch (err) {
         next(err);
     }
 });
+
 
 // دالة تسجيل الدخول للمشرف
 exports.loginAdmin = asyncHandler(async (req, res, next) => {
@@ -76,63 +67,19 @@ exports.Regestier = asyncHandler(async (req, res) => {
         password: req.body.password,
         company_name: req.body.company_name,
         telephone: req.body.telephone,
-        // role: req.body.role,
         country: req.body.country,
         state: req.body.state,
         city: req.body.city,
         area: req.body.area,
         street: req.body.street,
     })
+    createJWT(user, 'User registered successfully', 200, res);
 
-    // generate token
-    const token = createJWT(user._id)
-    res.status(200).send({ user, token })
 })
 
 // @desc   login user
 // @route  Post api/v1/Auth
 // @access Public
-
-// exports.login = asyncHandler(async (req, res, next) => {
-//     try {
-//         const { email, password } = req.body;
-//         const user = await User.findOne({ email });
-
-//         if (!user || !(await bcrypt.compare(password, user.password))) {
-//             return next(new ApiError('Invalid email or password', 401));
-//         }
-//         res.json("succefully login");
-//     } catch (err) {
-//         next(err);
-//     }
-// });
-
-
-// @desc   login admin
-// @route  Post api/v1/Auth
-// @access Public
-
-// exports.loginAdmin = asyncHandler(async (req, res, next) => {
-//     const { email, password, role } = req.body;
-//     if (!email || !password || !role) {
-//         return next(new ApiError("Please Fill Full Form!", 400));
-//     }
-//     const user = await User.findOne({ email });
-//     if (!user) {
-//         return next(new ApiError("Invalid email Or Password!1", 400));
-//     }
-//     if (password !== user.password) {
-//         return next(new ApiError("Invalid email Or Password!2", 400));
-//     }
-//     if (role !== user.role) {
-//         return next(new ApiError(`User Not Found With This Role!`, 400));
-//     }
-//     createJWT(user, "Login Successfully!", 201, res);
-// });
-
-
-
-
 
 exports.logout = asyncHandler(async (req, res, next) => {
     // قراءة التوكن من localStorage
